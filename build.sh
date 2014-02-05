5ba589534d7c208235eaac2498828766f57003cd
Yazhong Liu <l900422@vip.qq.com>
2014-02-05 13:28:08 +0800

    boop

--- build.sh
+++ build.sh
@@ -1,6 +1,17 @@
 #!/usr/bin/env bash
 
+# markdown part
+echo "start to generate .html from .md files"
+for path in posts/*.md ; do
+  name="${path%.*}"
+  md2html $path > $name.html
+done
+
+# browserify progress
+echo "start to browserify..."
 browserify    \
   -r assert   \
   -r markdown \
   lib/home.js > script/home.js
+
+echo "done!"
