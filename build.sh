92cce9b2dfab94fa7c3385b8ea6ae83dc1d293dc
Yazhong Liu <l900422@vip.qq.com>
2014-02-05 14:03:20 +0800

    generate date

--- build.sh
+++ build.sh
@@ -2,10 +2,21 @@
 
 # markdown part
 echo "start to generate .html from .md files"
+
+catalog='['
 for path in posts/*.md ; do
   name="${path%.*}"
+  date=`stat -f '%Sm' $path`
   md2html $path > $name.html
+  catalog=$catalog'
+  {
+    "name": "'$(basename $name)'",
+    "path": "'$name.html'",
+    "date": "'$date'"
+  }'
 done
+catalog=$catalog']'
+echo $catalog > catalog.json
 
 # browserify progress
 echo "start to browserify..."
